723f3a00684176a633045011cd160421c7ed7fe4
Ariston <aristoncatipay123@gmail.com>
2023-10-03 22:33:05 +0800

    Created the signup.html and register it to the urls.py.

--- core/views.py
+++ core/views.py
@@ -5,3 +5,8 @@ def index(request):
     return render(request, 'core/index.html', {
         'title': 'Welcome',
     })
+
+def signup(request):
+    return render(request, 'core/signup.html', {
+        'title': 'Signup',
+    })
