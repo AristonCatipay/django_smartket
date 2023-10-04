885b4ba57f95cda0e1026289427f6fa193f33e2c
Ariston <aristoncatipay123@gmail.com>
2023-10-04 11:07:51 +0800

    Deleted the product using metric unit app.

--- product_using_metric_unit/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductUsingMetricUnitConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'product_using_metric_unit'
