a20d1b51838c2814287176a6144a1b71e0bc3d66
Ariston <aristoncatipay123@gmail.com>
2023-10-05 14:20:04 +0800

    Created the add customer number functionality.

--- number/views.py
+++ number/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import AddCustomerNumber
 
 def index(request):
     return render(request, 'number/index.html', {
         'title': 'Customer Number'
+    })
+
+def add_customer_number(request):
+    if request.method == 'POST':
+        form = AddCustomerNumber(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('number:index')
+    else:
+        form = AddCustomerNumber()
+    return render(request, 'number/form.html', {
+        'title': 'Add Customer Number',
+        'form': form,
     })
