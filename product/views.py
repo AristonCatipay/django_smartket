b140742af8a378a8cd4187975421d442c306a33b
Ariston <aristoncatipay123@gmail.com>
2023-10-06 18:53:27 +0800

    Deleted the extra forms created for different actions in the product table. Created a single form for all actions.

--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from . forms import AddProduct, AddMetricUnit, EditProduct, EditMetricUnit, DeleteProduct, DeleteMetricUnit
+from . forms import ProductForm, AddMetricUnit, EditMetricUnit, DeleteMetricUnit
 from . models import Product, Metric_Unit
 
 def index(request):
@@ -9,25 +9,56 @@ def index(request):
         'products': products,
     })
 
-def metric(request):
-    metric_units = Metric_Unit.objects.all()
-    return render(request, 'product/index_metric_unit.html', {
-        'title': 'Metric',
-        'metric_units': metric_units,
-    })
-
 def add_product(request):
     if request.method == 'POST':
-        form = AddProduct(request.POST)
+        form = ProductForm(request.POST)
         
         if form.is_valid():
             form.save()
             return redirect('product:index')
     else:
-        form = AddProduct()
+        form = ProductForm()
     return render(request, 'product/form.html', {
         'title': 'Add Product', 
         'form': form
+    })
+
+def edit_product(request, primary_key):
+    product = get_object_or_404(Product, id=primary_key)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product:index')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'product/form.html', {
+        'title': 'Edit Product',
+        'form': form, 
+    })
+
+def delete_product(request, primary_key):
+    model = 'Product'
+    is_delete = True
+    product = get_object_or_404(Product, id=primary_key)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        product.delete()
+        return redirect('product:index')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'product/form.html', {
+        'title': 'Delete Product',
+        'form': form,
+        'is_delete': is_delete,
+        'model': model,
+    })
+
+def metric(request):
+    metric_units = Metric_Unit.objects.all()
+    return render(request, 'product/index_metric_unit.html', {
+        'title': 'Metric',
+        'metric_units': metric_units,
     })
 
 def add_metric(request):
@@ -43,19 +74,6 @@ def add_metric(request):
         'form': form,
     })
 
-def edit_product(request, primary_key):
-    product = get_object_or_404(Product, id=primary_key)
-    if request.method == 'POST':
-        form = EditProduct(request.POST, instance=product)
-        if form.is_valid():
-            form.save()
-            return redirect('product:index')
-    else:
-        form = EditProduct(instance=product)
-    return render(request, 'product/form.html', {
-        'title': 'Edit Product',
-        'form': form, 
-    })
 
 def edit_metric(request, primary_key):
     metric = get_object_or_404(Metric_Unit, id=primary_key)
@@ -72,22 +90,6 @@ def edit_metric(request, primary_key):
         'form': form,
     })
 
-def delete_product(request, primary_key):
-    model = 'Product'
-    is_delete = True
-    product = get_object_or_404(Product, id=primary_key)
-    if request.method == 'POST':
-        form = DeleteProduct(request.POST, instance=product)
-        product.delete()
-        return redirect('product:index')
-    else:
-        form = DeleteProduct(instance=product)
-    return render(request, 'product/form.html', {
-        'title': 'Delete Product',
-        'form': form,
-        'is_delete': is_delete,
-        'model': model,
-    })
 
 def delete_metric(request, primary_key):
     model = 'Metric Unit'
