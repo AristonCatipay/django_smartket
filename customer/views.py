d4f81e09c60a43866f37cb6d1705d68271976b7b
Ariston <aristoncatipay123@gmail.com>
2023-10-08 21:21:26 +0800

    Display customer data in the index page.

--- customer/views.py
+++ customer/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from . forms import CustomerForm
+from . models import Customer
 
 # Create your views here.
 
 def index(request):
+    customers = Customer.objects.all()
     return render(request, 'customer/index.html', {
         'title': 'Customer',
+        'customers': customers,
     })
 
 def add_customer(request):
