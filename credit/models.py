d669d436e327746d4753e0a9bd23541f04340d88
Ariston <aristoncatipay123@gmail.com>
2023-11-08 21:29:08 +0800

    Installed tailwind css.

--- credit/models.py
+++ credit/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+from customer.models import Customer
+from product.models import Product
+
+class Credited_Product(models.Model):
+    product = models.ForeignKey(Product, related_name='product_id', on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, related_name='customer_id_credit', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 
