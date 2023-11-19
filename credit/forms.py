e974a7ab3eddb5ef049db708a27182671bd246a5
Ariston <aristoncatipay123@gmail.com>
2023-11-19 22:16:53 +0800

    Create the CreditTransactionItemForm.

--- credit/forms.py
+++ credit/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Credit_Transaction, Credit_Transaction_Item
+from product.models import Product
 import datetime
 
 INPUT_CLASSES = 'block w-full p-2 text-sm text-gray-100 border border-violet-300 rounded-lg bg-gray-700 focus:ring-blue-500 focus:border-blue-500'
@@ -18,3 +19,17 @@ class CreditTransactionForm(forms.ModelForm):
             }),
         }
 
+class CreditTransactionItemForm(forms.ModelForm):
+    class Meta:
+        model = Credit_Transaction_Item
+        fields = ['product']
+        widgets = {
+            'product': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Set the choices for the product field
+        self.fields['product'].widget.choices = [(product.id, product.product_name) for product in Product.objects.all()]
