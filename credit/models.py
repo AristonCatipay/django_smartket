c22b6ca39f0a139f0f5d26cf2b30864d08dfb697
Ariston <aristoncatipay123@gmail.com>
2023-11-19 19:30:59 +0800

    Create the Credit Transaction and Credit Transaction Item model.
    Credit Transaction to represent the credited transaction as a whole.
    Credit Transaction Item to associate individual products with the credited transaction.

--- credit/models.py
+++ credit/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 from customer.models import Customer
 from product.models import Product
 
-class Credited_Product(models.Model):
-    product = models.ForeignKey(Product, related_name='product_id', on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer, related_name='customer_id_credit', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+class Credit_Transaction(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+    due_date = models.DateField()
+    paid_at = models.DateField(blank=True, null=True)
+    payment_delay = models.DurationField(blank=True, null=True)
 
+    def mark_as_paid(self):
+        self.is_paid = True
+        self.paid_at = timezone.now()
+        self.payment_delay = self.paid_at - self.due_date
+        self.save()
 
-
+class Credit_Transaction_Item(models.Model):
+    credit_transaction = models.ForeignKey(Credit_Transaction, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
