8dc7df0b5a3f5f2e81e05afb19cdbd2cef2c7e9f
Ariston <aristoncatipay123@gmail.com>
2023-11-19 20:08:07 +0800

    Create the index page of the credit app.

--- credit/views.py
+++ credit/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Credit_Transaction, Credit_Transaction_Item
 
 def index(request):
+    credit_transactions = Credit_Transaction.objects.all()
     return render(request, 'credit/index.html', {
         'title': 'Credit',
+        'credit_transactions': credit_transactions,
     })
+
+
+
+
+
+
+
