e0c764ea060d5568e589ec573a12b1b9a46d4b5c
Ariston <aristoncatipay123@gmail.com>
2023-11-20 14:03:28 +0800

    Create the edit credit product functionality.

--- credit/urls.py
+++ credit/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('transaction/paid/<int:credit_transaction_primary_key>/', views.mark_transaction_as_paid, name='mark_transaction_as_paid'),
     path('product/<int:credit_transaction_primary_key>/', views.credit_product, name='credit_product'),
     path('product/add/<int:credit_transaction_primary_key>/', views.add_credit_product, name='add_credit_product'),
+    path('product/edit/<int:credit_product_primary_key>/<int:credit_transaction_primary_key>/', views.edit_credit_product, name='edit_credit_product'),
 ]
--- credit/views.py
+++ credit/views.py
@@ -87,6 +87,28 @@ def add_credit_product(request, credit_transaction_primary_key):
         'form': form,
     })
 
+def edit_credit_product(request, credit_product_primary_key, credit_transaction_primary_key):
+    credit_product = get_object_or_404(Credit_Transaction_Item, pk=credit_product_primary_key)
+    if request.method == 'POST':
+        form = CreditTransactionItemForm(request.POST, instance=credit_product)
+        if form.is_valid():
+            credit_product = form.save(commit=False)
+
+            # Fetch the product from the form data
+            product = form.cleaned_data['product']
+            product = Product.objects.get(pk=product.id)
+            
+            credit_product.product = product
+            credit_product.product_current_price = product.product_price
+            credit_product.save()
+            return redirect('credit:credit_product', credit_transaction_primary_key)
+    else:
+        form = CreditTransactionItemForm(instance=credit_product)
+    return render(request, 'credit/form.html', {
+        'title': 'Edit Credit Product',
+        'form': form,
+    })
+
 
 
 
