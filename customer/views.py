3d6f47f83ab5fde43bd3143cba99b9327c7a07cd
Ariston <aristoncatipay123@gmail.com>
2023-11-20 18:36:16 +0800

    Only allow users to view customer pages if they are logged in.

--- customer/views.py
+++ customer/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from . forms import CustomerForm
 from . models import Customer
 
-# Create your views here.
-
+@login_required
 def index(request):
     query = request.GET.get('query', '')
     customers = Customer.objects.all()
@@ -17,6 +17,7 @@ def index(request):
         'customers': customers,
     })
 
+@login_required
 def add_customer(request):
     if request.method == 'POST':
         form = CustomerForm(request.POST)
@@ -31,6 +32,7 @@ def add_customer(request):
         'form': form,
     })
 
+@login_required
 def edit_customer(request, primary_key):
     customer = get_object_or_404(Customer, id=primary_key)
     if request.method == 'POST':
@@ -45,6 +47,7 @@ def edit_customer(request, primary_key):
         'form': form,
     })
 
+@login_required
 def delete_customer(request, primary_key):
     is_delete = True
     customer = get_object_or_404(Customer, id=primary_key)
