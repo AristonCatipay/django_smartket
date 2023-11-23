bd5690bb4ad2923b52dc95ceab0d43f69098d22e
Ariston <aristoncatipay123@gmail.com>
2023-11-23 17:40:21 +0800

    Update the design of input fields.

--- credit/forms.py
+++ credit/forms.py
@@ -3,7 +3,12 @@ from .models import Credit_Transaction, Credit_Transaction_Item
 from product.models import Product
 import datetime
 
-INPUT_CLASSES = 'block w-full p-2 text-sm text-gray-100 border border-violet-300 rounded-lg bg-gray-700 focus:ring-blue-500 focus:border-blue-500'
+INPUT_CLASSES = 'rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+SELECT_AREA = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+TEXT_AREA = 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+FOR_IMAGE = 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'
+CHECKBOX = 'w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+
 today = datetime.date.today()
 
 class CreditTransactionForm(forms.ModelForm):
@@ -12,10 +17,10 @@ class CreditTransactionForm(forms.ModelForm):
         fields = ['customer', 'due_date']
         widgets = {
             'customer': forms.Select(attrs={
-                'class': INPUT_CLASSES
+                'class': INPUT_CLASSES,
             }),
             'due_date': forms.SelectDateWidget(years=range(today.year, 2999), attrs={
-                'class': INPUT_CLASSES,
+                'class': SELECT_AREA,
             }),
         }
 
@@ -25,7 +30,7 @@ class CreditTransactionItemForm(forms.ModelForm):
         fields = ['product']
         widgets = {
             'product': forms.Select(attrs={
-                'class': INPUT_CLASSES
+                'class': SELECT_AREA
             }),
         }
 
