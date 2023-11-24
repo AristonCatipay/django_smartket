51da872f3231fff941f82fc0614747d7c7b10751
Ariston <aristoncatipay123@gmail.com>
2023-11-24 23:50:31 +0800

    Test the index page for the credit app urls.

---
+++ credit/tests/test_urls.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from credit.views import index, add_credit_transaction, edit_credit_transaction, mark_transaction_as_paid, credit_product, add_credit_product, edit_credit_product 
+
+class TestUrls(SimpleTestCase):
+    def test_index_url(self):
+        url = reverse('credit:index')
+        self.assertEquals(resolve(url).func, index)
