c6d02be7df33bb56d18861fc63a9e7ce174870bc
Ariston <aristoncatipay123@gmail.com>
2023-11-26 23:10:02 +0800

    Create test for core home view function.

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -6,6 +6,12 @@ class CoreViewTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
 
+    def test_home_view(self):
+        url = reverse('core:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response ,'core/home.html')
+
     def test_index_view(self):
         self.client.force_login(self.user)
         url = reverse('core:index')
