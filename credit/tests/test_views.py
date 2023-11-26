54bd3762fc3d0411ea99a63b8f5c86429efa4c69
Ariston <aristoncatipay123@gmail.com>
2023-11-26 19:49:58 +0800

    Create test for edit credit transaction page.

--- credit/tests/test_views.py
+++ credit/tests/test_views.py
@@ -9,6 +9,7 @@ from datetime import date
 class CreditViewsTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
+        
         self.customer = Customer.objects.create(
             first_name='John',
             last_name='Doe',
@@ -19,6 +20,13 @@ class CreditViewsTestCase(TestCase):
             street='123 Main St',
             city='Some City',
             birth_date=date(1993, 5, 15),
+        )
+
+        self.credit_transaction = Credit_Transaction.objects.create(
+            customer=self.customer,
+            created_by=self.user,
+            due_date=date(2023, 12, 31),
+            is_paid=False
         )
 
     def test_index_view(self):
@@ -38,6 +46,20 @@ class CreditViewsTestCase(TestCase):
         data = {
             'customer': self.customer.id,
             'due_date': '2023-12-31',
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_edit_credit_transaction_view(self):
+        self.client.force_login(self.user)
+        url = reverse('credit:edit_credit_transaction', kwargs={'credit_transaction_primary_key': self.credit_transaction.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'credit/form.html')
+
+        data = {
+            'customer': self.credit_transaction.customer.id,
+            'due_date': self.credit_transaction.due_date,
         }
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, 302)
