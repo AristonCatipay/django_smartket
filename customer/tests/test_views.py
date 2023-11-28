a3838bf91863d83c077acda19cded78d8b2effb1
Ariston <aristoncatipay123@gmail.com>
2023-11-28 14:05:53 +0800

    Create test for delete customer view function.

--- customer/tests/test_views.py
+++ customer/tests/test_views.py
@@ -81,6 +81,33 @@ class CustomerViewTestCase(TestCase):
 
         # Print data for inspection
         print("\nTest Data Used (Edit Customer):", data, "\n")
+    
+    def test_delete_customer(self):
+        self.client.force_login(self.user)
+        url = reverse('customer:delete_customer', kwargs={'primary_key': self.customer.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'customer/form.html')
+
+        data = {
+            'first_name': self.customer.first_name,
+            'last_name': self.customer.last_name,
+            'username': self.customer.username,
+            'age': self.customer.age,
+            'gender': self.customer.gender,
+            'email': self.customer.email,
+            'civil_status': self.customer.civil_status,
+            'street': self.customer.street,
+            'barangay': self.customer.barangay,
+            'city': self.customer.city,
+            'birth_date': self.customer.birth_date,
+        }
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+
+        # Print data for inspection
+        print("\nTest Data Used (Delete Customer):", data, "\n")
 
     def tearDown(self):
         # Cleanup after each test
