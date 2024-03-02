d826fb19dbb68b4be876a82c5b3f7fff00d07a0b
Ariston <aristoncatipay123@gmail.com>
2024-03-02 20:58:29 +0800

    Create the update region functionality.

--- address/urls.py
+++ address/urls.py
@@ -6,4 +6,5 @@ app_name = 'address'
 urlpatterns = [
     path('region/', views.view_region, name='view_region'),
     path('region/create/', views.create_region, name='create_region'),
+    path('region/update/<int:primary_key>/', views.update_region, name='update_region'),
 ]
