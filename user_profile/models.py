acf1543e664fb87e08de495f1cb25fa3c10f7d2e
Ariston <aristoncatipay123@gmail.com>
2024-03-03 22:58:08 +0800

    Create the update user address functionality.

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from address.models import Region, Province, City_Municipality, Barangay
  
 class Profile(models.Model):
     MALE = 'M'
@@ -15,3 +16,11 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile_images', default='default_profile_image.jpg')
     gender = models.CharField(max_length=20, choices=GENDER_CHOICES, default=OTHERS)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100, blank=True)
+    barangay = models.ForeignKey(Barangay, on_delete=models.SET_NULL, null=True)
+    city_municipality = models.ForeignKey(City_Municipality, on_delete=models.SET_NULL, null=True)
+    province = models.ForeignKey(Province, on_delete=models.SET_NULL, null=True)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.user.username
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,4 +7,8 @@ urlpatterns=[
     path('', views.view_profile, name='view_profile'),
     path('update/', views.update_profile, name='update_profile'),
     path('password/update/', views.update_password, name='update_password'),
+    path('address/update/', views.update_address, name='update_address'),
+    path('address/update/province/load/', views.load_province, name='load_province'),
+    path('address/update/city/municipality/load/', views.load_city_municipality, name='load_city_municipality'),
+    path('address/update/barangay/load/', views.load_barangay, name='load_barangay'),
 ]
