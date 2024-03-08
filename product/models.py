70606aa88d33e33acf3995f5cf5414054bf5a700
Ariston <aristoncatipay123@gmail.com>
2024-03-08 22:52:47 +0800

    Create a functionality that would automatically add predefined records for the category table when you migrate

--- product/models.py
+++ product/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 class Metric_Unit(models.Model):
     name = models.CharField(max_length=255)
@@ -19,22 +21,32 @@ class Category(models.Model):
     # Showing the actual name of the field.
     def __str__(self):
         return self.name
-    
-    # Different Categories
-    # Can goods
-    # Sauces
-    # Drinks and Beverages
-    # Coffee and Sugar
-    # Frozen Food
-    # Milk and Dairy
-    # Noodles
-    # Cup Noodles
-    # Soup and Shampoo
-    # Chemicals
-    # Medicine
-    # Crackers
-    # Diaper
-    # Not Specified
+
+# Predefined categories
+PREDEFINED_CATEGORIES = [
+    "Can Goods",
+    "Sauces",
+    "Drinks and Beverages",
+    "Coffee and Sugar",
+    "Frozen Food",
+    "Milk and Dairy",
+    "Noodles",
+    "Cup Noodles",
+    "Soup and Shampoo",
+    "Chemicals",
+    "Medicine",
+    "Crackers",
+    "Diaper",
+    "Not Specified"
+]
+
+@receiver(post_migrate)
+def check_categories(sender, **kwargs):
+    # Check if Category table is empty
+    if Category.objects.count() == 0:
+        # If empty, populate with predefined categories
+        for category_name in PREDEFINED_CATEGORIES:
+            Category.objects.create(name=category_name)
 
 class Color(models.Model):
     name = models.CharField(max_length=255)
